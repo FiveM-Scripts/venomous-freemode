ecc1fc8f40275c2d83817ccb8349c574119f44fa
Emre <pongo1999712@gmail.com>
2018-09-12 22:45:23 +0200

    Some playerlist app progress

--- [phone]/vf_baseapps/apps/cl_appplayerlist.lua
+++ [phone]/vf_baseapps/apps/cl_appplayerlist.lua
@@ -1,15 +1,59 @@
 AppPlayerlist = {
     AppName = GetLabelText("CELL_35"),
-    AppIcon = 14
+    AppIcon = 11
 }
+local phoneScaleform
 local selectedItem
 
-function AppPlayerlist.Init()
+function AppPlayerlist.Init(scaleform)
+    phoneScaleform = scaleform
     selectedItem = 0
 end
 
 function AppPlayerlist.Tick()
+    PushScaleformMovieFunction(phoneScaleform, "SET_DATA_SLOT_EMPTY")
+    PushScaleformMovieFunctionParameterInt(13)
+    PopScaleformMovieFunctionVoid()
 
+    local players = {}
+    for i = 0, 64 do
+        if NetworkIsPlayerConnected(i) then
+            PushScaleformMovieFunction(phoneScaleform, "SET_DATA_SLOT")
+            PushScaleformMovieFunctionParameterInt(13)
+            PushScaleformMovieFunctionParameterInt(i)
+            PushScaleformMovieFunctionParameterInt()
+            PushScaleformMovieFunctionParameterString(GetPlayerName(i))
+            PopScaleformMovieFunctionVoid()
+
+            table.insert(players, i)
+        end
+    end
+
+    PushScaleformMovieFunction(phoneScaleform, "DISPLAY_VIEW")
+    PushScaleformMovieFunctionParameterInt(13)
+    PushScaleformMovieFunctionParameterInt(selectedItem)
+
+    local navigated = true
+    if IsControlJustPressed(0, 300) then
+        if selectedItem > 0 then
+            selectedItem = selectedItem - 1
+        else
+            selectedItem = #players - 1
+        end
+    elseif IsControlJustPressed(0, 299) then
+        if selectedItem < #players - 1 then
+            selectedItem = selectedItem + 1
+        else
+            selectedItem = 0
+        end
+    elseif IsControlJustPressed(0, 255) then
+        
+    else
+        navigated = false
+    end
+    if navigated then
+        PlaySoundFrontend(-1, "Menu_Navigate", "Phone_SoundSet_Default")
+    end
 end
 
 AddEventHandler("vf_phone:setup", function()
