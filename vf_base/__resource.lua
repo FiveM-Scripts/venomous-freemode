7a27cb3c9abdfd64e3c62f060e891ea0322c2c55
Glenn Hermans <glhermans@gmail.com>
2018-10-01 03:59:06 +0200

    Updated resource file

--- vf_base/__resource.lua
+++ vf_base/__resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 resource_type 'gametype' { name = 'venomous-freemode' }
 resource_version '1.0'
 
-dependencies {'mysql-async', 'NativeUI'}
+dependencies {'ghmattimysql', 'NativeUI'}
 
 client_scripts {
     '@NativeUI/NativeUI.lua',
@@ -15,9 +15,9 @@ client_scripts {
 }
 
 server_scripts {
-    '@mysql-async/lib/MySQL.lua',
     'config/freemode.lua',
     'server/database.lua',
     'server/player.lua',
+    'server/commands.lua',
     'server/general.lua'
 }
