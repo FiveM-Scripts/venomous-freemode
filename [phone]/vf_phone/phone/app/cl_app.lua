fef7f5d66ca7a1c7e9b0dba35c8f1cfc86e96b25
Emre Cürgül <pongo1999712@gmail.com>
2018-10-30 17:33:23 +0000

    vf_phone: Revert tworking screen if screen is removed while being shown (RESOURCE BROKEN CURRENTLY)

--- [phone]/vf_phone/phone/app/cl_app.lua
+++ [phone]/vf_phone/phone/app/cl_app.lua
@@ -1,18 +1,23 @@
 Apps = {}
-local selectedItem
+local _SelectedItem
+local _CurrentApp
+local _CurrentScreen
+local _PrevScreens
 
-function Apps.Start(app)
-    if Apps[app] and Apps[app].LauncherScreen then
-        Apps.CurrentApp = Apps[app]
-        Apps.PrevScreens = {}
-        Apps.CurrentScreen = Apps[app].LauncherScreen
-        selectedItem = 0
+function Apps.Start(appId)
+    if Apps[appId] and Apps[appId].LauncherScreen then
+        _CurrentApp = Apps[appId]
+        _CurrentScreen = Apps[appId].LauncherScreen
+        _PrevScreens = {}
+        _SelectedItem = 0
+        Phone.InApp = true
         PlaySoundFrontend(-1, "Menu_Navigate", "Phone_SoundSet_Default")
     end
 end
 
 function Apps.Kill()
-    Apps.CurrentApp = nil
+    Phone.InApp = false
+    _CurrentApp = nil
     PlaySoundFrontend(-1, "Hang_Up", "Phone_SoundSet_Michael")
 end
 
@@ -20,87 +25,104 @@ Citizen.CreateThread(function()
     while true do
         Wait(0)
 
-        if Apps.CurrentApp then
-            PushScaleformMovieFunction(Phone.Scaleform, "SET_DATA_SLOT_EMPTY")
-            PushScaleformMovieFunctionParameterInt(Apps.CurrentScreen.Type)
-            PopScaleformMovieFunctionVoid()
-
-            local header
-            if Apps.CurrentScreen.Header then
-                header = Apps.CurrentScreen.Header
-            else
-                header = Apps.CurrentApp.Name
-            end
-            PushScaleformMovieFunction(Phone.Scaleform, "SET_HEADER")
-            PushScaleformMovieFunctionParameterString(header)
-            PopScaleformMovieFunctionVoid()
-
-            for i, item in ipairs(Apps.CurrentScreen.Items) do
-                PushScaleformMovieFunction(Phone.Scaleform, "SET_DATA_SLOT")
-                PushScaleformMovieFunctionParameterInt(Apps.CurrentScreen.Type)
-                PushScaleformMovieFunctionParameterInt(i - 1)
-                for _, data in ipairs(item.Data) do
-                    if type(data) == "number" then
-                        if math.type(data) == "integer" then
-                            PushScaleformMovieFunctionParameterInt(data)
-                        else
-                            PushScaleformMovieFunctionParameterFloat(data)
-                        end
-                    elseif type(data) == "string" then
-                        PushScaleformMovieFunctionParameterString(data)
-                    elseif not data then
-                        PushScaleformMovieFunctionParameterInt()
-                    end
-                end
-                PopScaleformMovieFunctionVoid()
-            end
-        
-            PushScaleformMovieFunction(Phone.Scaleform, "DISPLAY_VIEW")
-            PushScaleformMovieFunctionParameterInt(Apps.CurrentScreen.Type)
-            PushScaleformMovieFunctionParameterInt(selectedItem)
-        
-            -- Fix selectedItem in case last item got removed while it was selected
-            if selectedItem > #Apps.CurrentScreen.Items - 1 then
-                selectedItem = #Apps.CurrentScreen.Items - 1
-            end
-
-            local navigated = true
-            if IsControlJustPressed(0, 300) then -- Up
-                selectedItem = selectedItem - 1
-                if selectedItem < 0 then
-                    selectedItem = #Apps.CurrentScreen.Items - 1
-                end
-            elseif IsControlJustPressed(0, 299) then -- Down
-                selectedItem = selectedItem + 1
-                if selectedItem > #Apps.CurrentScreen.Items - 1 then
-                    selectedItem = 0
-                end
-            elseif IsControlJustPressed(0, 255) then -- Enter
-                if #Apps.CurrentScreen.Items > 0 then
-                    local item = Apps.CurrentScreen.Items[selectedItem + 1]
-                    if type(item.Callback) == "table" then -- Action (Should be function, but it isn't because it's a table according to Msgpack!)
-                        item.Callback()
-                    elseif type(item.Callback) == "number" then -- Screen
-                        table.insert(Apps.PrevScreens, Apps.CurrentScreen)
-                        Apps.CurrentScreen = Apps.CurrentApp.Screens[item.Callback]
-                        selectedItem = 0
-                    end
-                end
-            elseif IsControlJustPressed(0, 202) then -- Back
-                if #Apps.PrevScreens > 0 then
-                    Apps.CurrentScreen = Apps.PrevScreens[#Apps.PrevScreens]
-                    table.remove(Apps.PrevScreens)
-                    selectedItem = 0
-                else
-                    Wait(0) -- Workaround to stop main app from registering back press too
+        if _CurrentApp then
+            if not _CurrentScreen then -- Revert back to existing screen if screen got removed
+                if not _PrevScreens then -- Quit app if no screens exist
                     Apps.Kill()
+                else
+                    local screenFound = false
+                    for _, screen in ipairs(_PrevScreens) do
+                        if screen and not screenFound then
+                            _CurrentScreen = screen
+                            screenFound = true
+                        end
+                    end
+                    if not screenFound then -- No existing screen found
+                        Apps.Kill()
+                    end
+                end
+            else
+                PushScaleformMovieFunction(Phone.Scaleform, "SET_DATA_SLOT_EMPTY")
+                PushScaleformMovieFunctionParameterInt(_CurrentScreen.Type)
+                PopScaleformMovieFunctionVoid()
+
+                local header
+                if _CurrentScreen.Header then
+                    header = _CurrentScreen.Header
+                else
+                    header = _CurrentApp.Name
+                end
+                PushScaleformMovieFunction(Phone.Scaleform, "SET_HEADER")
+                PushScaleformMovieFunctionParameterString(header)
+                PopScaleformMovieFunctionVoid()
+
+                for i, item in ipairs(_CurrentScreen.Items) do
+                    PushScaleformMovieFunction(Phone.Scaleform, "SET_DATA_SLOT")
+                    PushScaleformMovieFunctionParameterInt(_CurrentScreen.Type)
+                    PushScaleformMovieFunctionParameterInt(i - 1)
+                    for _, data in ipairs(item.Data) do
+                        if type(data) == "number" then
+                            if math.type(data) == "integer" then
+                                PushScaleformMovieFunctionParameterInt(data)
+                            else
+                                PushScaleformMovieFunctionParameterFloat(data)
+                            end
+                        elseif type(data) == "string" then
+                            PushScaleformMovieFunctionParameterString(data)
+                        elseif not data then
+                            PushScaleformMovieFunctionParameterInt()
+                        end
+                    end
+                    PopScaleformMovieFunctionVoid()
+                end
+            
+                PushScaleformMovieFunction(Phone.Scaleform, "DISPLAY_VIEW")
+                PushScaleformMovieFunctionParameterInt(_CurrentScreen.Type)
+                PushScaleformMovieFunctionParameterInt(_SelectedItem)
+            
+                -- Fix _SelectedItem in case last item got removed while it was selected
+                if _SelectedItem > #_CurrentScreen.Items - 1 then
+                    _SelectedItem = #_CurrentScreen.Items - 1
+                end
+
+                local navigated = true
+                if IsControlJustPressed(0, 300) then -- Up
+                    _SelectedItem = _SelectedItem - 1
+                    if _SelectedItem < 0 then
+                        _SelectedItem = #_CurrentScreen.Items - 1
+                    end
+                elseif IsControlJustPressed(0, 299) then -- Down
+                    _SelectedItem = _SelectedItem + 1
+                    if _SelectedItem > #_CurrentScreen.Items - 1 then
+                        _SelectedItem = 0
+                    end
+                elseif IsControlJustPressed(0, 255) then -- Enter
+                    if #_CurrentScreen.Items > 0 then
+                        local item = _CurrentScreen.Items[_SelectedItem + 1]
+                        if type(item.Callback) == "table" then -- Action (Should be function, but it isn't because it's a table according to Msgpack!)
+                            item.Callback()
+                        elseif type(item.Callback) == "number" then -- Screen
+                            table.insert(_PrevScreens, _CurrentScreen)
+                            _CurrentScreen = _CurrentApp.Screens[item.Callback]
+                            _SelectedItem = 0
+                        end
+                    end
+                elseif IsControlJustPressed(0, 202) then -- Back
+                    if #_PrevScreens > 0 then
+                        _CurrentScreen = _PrevScreens[#_PrevScreens]
+                        table.remove(_PrevScreens)
+                        _SelectedItem = 0
+                    else
+                        Wait(0) -- Workaround to stop main app from registering back press too
+                        Apps.Kill()
+                        navigated = false
+                    end
+                else
                     navigated = false
                 end
-            else
-                navigated = false
-            end
-            if navigated then
-                PlaySoundFrontend(-1, "Menu_Navigate", "Phone_SoundSet_Default")
+                if navigated then
+                    PlaySoundFrontend(-1, "Menu_Navigate", "Phone_SoundSet_Default")
+                end
             end
         end
     end
