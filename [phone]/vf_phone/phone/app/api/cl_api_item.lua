b6860bd5e5b2b3825b425d75cab79748c2825a46
pongo1231 <pongo1999712@gmail.com>
2019-09-15 17:25:29 +0200

    vf_phone: Change no items screen deletion logic

--- [phone]/vf_phone/phone/app/api/cl_api_item.lua
+++ [phone]/vf_phone/phone/app/api/cl_api_item.lua
@@ -11,30 +11,37 @@ local function _CreateBaseItem(screen, data, callback)
     return item
 end
 
-function Item.RemoveItem(app, screen, item)
+function Item.RemoveItem(app, screen, item, doNotRemoveScreen)
     local itemIndex
     -- Check if screen isn't being used anywhere else
-    local removeScreen = true
+    local removeScreen = not doNotRemoveScreen
     for _, screen in ipairs(app.Screens) do
         for i, screenItem in ipairs(screen.Items) do
             if screenItem == item then
                 itemIndex = i
+
+                -- Don't bother continuing if removeScreen is false already
+                if not removeScreen then
+                    break
+                end
             elseif screenItem.Callback == item.Callback then
                 removeScreen = false
             end
         end
     end
+
     -- Remove screen if possible
     if removeScreen then
         table.remove(app.Screens, item.Callback)
     end
+    
     -- Remove item
     table.remove(screen.Items, itemIndex)
 end
 
 function Item.RemoveItemsInScreen(app, screen)
     for i = #screen.Items, 1, -1 do -- Reverse looping for safe table content removal
-        Item.RemoveItem(app, screen, screen.Items[i])
+        Item.RemoveItem(app, screen, screen.Items[i], true)
     end
 end
 
