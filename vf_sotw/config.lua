d2916648f66010b13783be1268340bd26f78e8e6
ghermans <glhermans@gmail.com>
2019-10-02 00:29:24 +0200

    Added store: smoke on the water

---
+++ vf_sotw/config.lua
@@ -0,0 +1,17 @@
+-- Configure the weed products
+weedItems = {
+    {text="Amnesia Haze", price=12},
+    {text="Blue Cheese", price=15},
+    {text="Bubblegum", price=9},
+    {text="Cheeze Haze", price=9},
+    {text="Dr.Grinspoon", price=18},
+    {text="G13 Haze", price=15},
+    {text="Liberty Haze", price=17},
+    {text="Mexican Haze", price=12},
+    {text="N.L.X", price=6},
+    {text="Silver", price=12},
+    {text="Smoke mix", price=5},
+    {text="Utopia Kush", price=17},
+    {text="Vanilla Kush", price=15},
+    {text="White Widow", price=9},
+}
