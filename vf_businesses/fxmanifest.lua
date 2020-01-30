7f1e451a04a57b62b5e26053d1a8780324ed3531
Glenn Hermans <glhermans@gmail.com>
2020-01-30 16:01:35 +0100

    Added init businesses resource

---
+++ vf_businesses/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version '44febabe-d386-4d18-afbe-5e627f4af937'
+game 'gta5'
+dependency 'ghmattimysql'
+
+client_scripts {
+    'config.lua',
+    'client/cl_businesses.lua'
+}
+
+server_script 'server/sv_businesses.lua'
