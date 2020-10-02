ce112e0603233c6b8059b75be06a5fb04e37c985
Glenn Hermans <glhermans@gmail.com>
2020-10-02 15:53:52 +0200

    update

--- vf_ammunation/client.lua
+++ vf_ammunation/client.lua
@@ -1,3 +1,21 @@
+--[[
+            vf_ammunation - Venomous Freemode - ammunation stores resource
+              Copyright (C) 2018-2020  FiveM-Scripts
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program in the file "LICENSE".  If not, see <http://www.gnu.org/licenses/>.
+]]
+
 local blips = {
   {x=1701.292, y=3750.450, z=34.365},
   {x=237.428, y=-43.655, z=69.698},
